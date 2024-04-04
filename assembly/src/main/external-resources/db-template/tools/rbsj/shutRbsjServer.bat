095d947c9a77f851e13e0d46abbd314af698077d
sourcekraft <virgil.trasca@gmail.com>
2024-04-04 14:02:52 +0200

    ----

--- assembly/src/main/external-resources/db-template/tools/rbsj/shutRbsjServer.bat
+++ assembly/src/main/external-resources/db-template/tools/rbsj/shutRbsjServer.bat
@@ -2,9 +2,9 @@
 REM Stop/kill any running java server
 powershell -Command "Get-WmiObject Win32_Process -Filter \"name = 'java.exe'\" | Where-Object { $_.CommandLine -like '*rb-server.jar*' } | ForEach-Object { Stop-Process -Id $_.ProcessId }"
 
-REM Empty the ../logs/electron.log file
-powershell -Command "Set-Content -Path '../logs/electron.log' -Value ''"
+REM Empty the ../../logs/electron.log file
+powershell -Command "Set-Content -Path '../../logs/electron.log' -Value ''"
 
-REM Empty the ../temp directory
-powershell -Command "Get-ChildItem -Path '../temp/*' | Remove-Item -Recurse -Force"
+REM Empty the ../../temp directory
+powershell -Command "Get-ChildItem -Path '../../temp/*' | Remove-Item -Recurse -Force"
 :: pause
