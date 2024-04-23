b46a427ec72de37f4a34fd1dc7f542ad80ae51ec
sourcekraft <virgil.trasca@gmail.com>
2024-04-23 12:42:50 +0200

    ----

--- assembly/src/main/external-resources/db-template/tools/rbsj/shutRbsjServer.bat
+++ assembly/src/main/external-resources/db-template/tools/rbsj/shutRbsjServer.bat
@@ -1,36 +1,37 @@
 @echo off
-:: Check if FRONTEND_PATH is set and points to a valid directory
-if exist "%FRONTEND_PATH%" (
-    :: Find the file that contains the server port number
-    for /R %%G in (server-*.port) do (
-        set "FILE=%%~nG"
-        goto :foundServer
-    )
-    echo No server port file found
-    exit /b 1
-    :foundServer
-
-    :: Extract the server port number from the file name
-    set "PORT=%FILE:~7,-5%"
-) else (
-    :: Find the file that contains the exe port number
-    for /R %%G in (exe-*.port) do (
-        set "FILE=%%~nG"
-        goto :foundExe
-    )
-    echo No exe port file found
-    exit /b 1
-    :foundExe
-
-    :: Extract the exe port number from the file name
-    set "PORT=%FILE:~4,-5%"
+:: Find the file that contains the server port number
+for /R %%G in (server-*.port) do (
+    set "FILE=%%~nG"
+    goto :foundServer
+)
+:: If no server port file found, check for exe port file
+for /R %%G in (exe-*.port) do (
+    set "FILE=%%~nG"
+    goto :foundExe
 )
 
-:: Use the port number as the unique identifier
-set "UID=%PORT%"
+:: If no server or exe port file found, stop the Java process without checking for the UID
+powershell -Command "Get-WmiObject Win32_Process -Filter \"name = 'java.exe'\" | Where-Object { $_.CommandLine -like '*rb-server.jar*' } | ForEach-Object { Stop-Process -Id $_.ProcessId }"
+goto :eof
 
-:: Stop the Java process
-powershell -Command "Get-WmiObject Win32_Process -Filter \"name = 'java.exe'\" | Where-Object { $_.CommandLine -like '*rb-server.jar*' -and $_.CommandLine -like '*%UID%*' } | ForEach-Object { Stop-Process -Id $_.ProcessId }"
+:foundServer
+:: Extract the server port number from the file name
+set "PORT=%FILE:~7,-5%"
+goto :continue
 
-:: Delete the port file
-del "%FILE%.port"
+:foundExe
+:: Extract the exe port number from the file name
+set "PORT=%FILE:~4,-5%"
+
+:continue
+:: Check if the port file exists
+if exist "%FILE%.port" (
+    :: Use the port number as the unique identifier
+    set "UID=%PORT%"
+
+    :: Stop the Java process
+    powershell -Command "Get-WmiObject Win32_Process -Filter \"name = 'java.exe'\" | Where-Object { $_.CommandLine -like '*rb-server.jar*' -and $_.CommandLine -like '*%UID%*' } | ForEach-Object { Stop-Process -Id $_.ProcessId }"
+
+    :: Delete the port file
+    del "%FILE%.port"
+)
