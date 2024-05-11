f4db1bff3dea24763ea0d1376deb843e1c9e7a72
sourcekraft <virgil.trasca@gmail.com>
2024-05-11 19:25:29 +0200

    ----

--- xtra-tools/build/common-scripts/maven/lib-repository/install-jars.cmd
+++ xtra-tools/build/common-scripts/maven/lib-repository/install-jars.cmd
@@ -5,9 +5,6 @@
 
 :: Step 1 - install jpoller1.5.2 - Directory Poller
 
-call mvn install:install-file -Dfile=./common/org.sadun.util.jar -DgroupId=org.sadun -DartifactId=org.sadun.util -Dversion=1.5.2 -Dpackaging=jar
-call mvn install:install-file -Dfile=./common/pollmgt.jar -DgroupId=org.sadun -DartifactId=pollmgt -Dversion=1.5.2 -Dpackaging=jar
-
 :: Step 2 - install Pherialize
 
 call mvn install:install-file -Dfile=./burst/pherialize-1.2.1.jar -DgroupId=de.ailis.pherialize -DartifactId=pherialize -Dversion=1.2.1 -Dpackaging=jar
