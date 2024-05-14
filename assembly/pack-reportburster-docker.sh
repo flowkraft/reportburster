cd119f197df24528f1262db6dd22cb05a144ff04
sourcekraft <virgil.trasca@gmail.com>
2024-05-14 18:47:39 +0200

    -----

--- assembly/pack-reportburster-docker.sh
+++ assembly/pack-reportburster-docker.sh
@@ -1,1 +1,7 @@
+rsync -a ../backend/reporting/src/main/external-resources/template/ ../template-merged-directory/
+rsync -a ../assembly/src/main/external-resources/db-template/ ../template-merged-directory/
+rsync -a ../assembly/src/main/external-resources/db-server-template/ ../template-merged-directory/
+
 docker build --no-cache --progress=plain -t reportburster_server:10.1.1 -f ../Dockerfile ..
+
+rm -rf ../template-merged-directory/
