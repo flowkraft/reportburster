28e4adcdc5b8d20c991995239cebdd1f553b5497
sourcekraft <virgil.trasca@gmail.com>
2024-05-15 17:38:58 +0200

    ------

--- assembly/pack-reportburster-linux.sh
+++ assembly/pack-reportburster-linux.sh
@@ -8,21 +8,24 @@ version=$(grep -oPm1 "(?<=<version>)[^<]+" ../backend/reporting/src/main/externa
 
 # Step 3: Build the Docker image and upload it to Docker Hub
 docker build --no-cache --progress=plain -t reportburster_server:$version -f ../Dockerfile ..
-docker push reportburster_server:$version
+# docker push reportburster_server:$version
 
-# Step 4: Pack docker folder as a zip file
+# Step 4: Update docker-compose.yml and  with the version parsed previously
+sed -i "s/reportburster_server:.*/reportburster_server:$version/" ./docker/docker-compose.yml
+
+# Update the startServer.sh startServer.sh with the latest version
+sed -i "s/\(image=\"reportburster_server:\).*\"/\1$version\"/" ./docker/startServer.sh
+
+chmod +x ./docker/startServer.sh
+chmod +x ./docker/shutServer.sh
+
+# Step 5: Pack docker folder as a zip file
 rm -rf ./docker-temp/
 mkdir ./docker-temp
 
 cp ./docker/docker-compose.yml ./docker-temp/docker-compose.yml 
 cp ./docker/startServer.sh ./docker-temp/startServer.sh 
 cp ./docker/shutServer.sh ./docker-temp/shutServer.sh 
-
-# Step 5: Update docker-compose.yml and  with the version parsed previously
-sed -i "s/reportburster_server:.*/reportburster_server:$version/" ./docker-temp/docker-compose.yml
-
-# Update the startServer.sh startServer.sh with the latest version
-sed -i "s/\(image=\"reportburster_server:\).*\"/\1$version\"/" ./docker-temp/startServer.sh
 
 chmod +x ./docker-temp/startServer.sh
 chmod +x ./docker-temp/shutServer.sh
@@ -33,4 +36,4 @@ zip -r ./dist/reportburster-server-linux.zip ./docker-temp
 rm -rf ./docker-temp/
 
 # Step 6: Use rclone to sync/upload the newly generated local zip file to s3
-rclone sync ./dist/reportburster-server-linux.zip s3:your_bucket_name
+# rclone sync ./dist/reportburster-server-linux.zip s3:your_bucket_name
