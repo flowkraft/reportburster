46466573b073d363bd605324218329c0705d0737
sourcekraft <virgil.trasca@gmail.com>
2024-05-23 01:00:18 +0200

    -----

--- assembly/src/user-acceptance/resources/vars.py
+++ assembly/src/user-acceptance/resources/vars.py
@@ -7,11 +7,11 @@ cwd = os.getcwd()
 # create signal_service and point it to correct version of preinstalled chromedriver
 # version should match your electron framework and those can be downloaded from:
 # https://github.com/electron/electron/releases under the version, from assets.
-chromedriver_path = os.path.join(os.path.dirname(os.path.dirname(cwd)), "frontend/reporting/node_modules/electron-chromedriver/bin/chromedriver.exe")
+chromedriver_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(cwd))), "frontend/reporting/node_modules/electron-chromedriver/bin/chromedriver.exe")
 signal_service = ChromeService(executable_path=chromedriver_path)
 
 # And here we construct options that point to use your actual electron app.
-PORTABLE_EXECUTABLE_DIR = os.path.join(os.path.dirname(cwd), "target/uat/ReportBurster")
+PORTABLE_EXECUTABLE_DIR = os.path.join(os.path.dirname(os.path.dirname(cwd)), "target/user-acceptance/rb/ReportBurster")
 
 reportburster_exe_path = os.path.join(PORTABLE_EXECUTABLE_DIR, "ReportBurster.exe")
 signal_electron = Options()
