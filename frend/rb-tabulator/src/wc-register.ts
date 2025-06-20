8de0fde40a64afa66d5b8fb726358c340544cf2f
sourcekraft <virgil.trasca@gmail.com>
2025-06-20 08:44:23 +0200

    Add rb-tabulator

---
+++ frend/rb-tabulator/src/wc-register.ts
@@ -0,0 +1,3 @@
+// This file explicitly registers all web components
+import "./lib/BasicTest.wc.svelte";
+console.log("wc-register.ts: Basic test component imported");
---
+++ frend/rb-tabulator/src/wc/MyComponent.wc.svelte
@@ -0,0 +1,15 @@
+<svelte:options customElement="my-component" />
+
+<script lang="ts">
+  export let name: string = 'World';
+</script>
+
+<style>
+  p {
+    color: blue;
+    font-family: sans-serif;
+    outline: solid 1px red;
+  }
+</style>
+
+<p>Hello, {name}!</p>
