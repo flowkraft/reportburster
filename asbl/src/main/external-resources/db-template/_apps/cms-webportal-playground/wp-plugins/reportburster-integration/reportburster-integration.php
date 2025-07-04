44da1fda3ae530e32ba1986b10485020830aa0db
sourcekraft <virgil.trasca@gmail.com>
2025-07-04 11:48:48 +0200

    SQL Query & Script report input types part 15

--- asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/reportburster-integration.php
+++ asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/reportburster-integration.php
@@ -1,20 +1,23 @@
 <?php
 
 /**
- * Plugin Name: ReportBurster Integration Blade Boilerplate
- * Plugin URI: https://github.com/wpbones/ReportBurster_Integration-Boilerplate
- * Description: WP Bones Blade Boilerplate WordPress plugin
- * Version: 1.9.6
+ * Plugin Name: ReportBurster Integration
+ * Plugin URI: https://reportburster.com/
+ * Description: ReportBurster Integration for WordPress, a boilerplate plugin to integrate ReportBurster with WordPress.
+ * Version: 1.0.0
  * Requires at least: 6.2
  * Requires PHP: 7.4
- * Author: Giovambattista Fazioli
- * Author URI: https://wpbones.com/
+ * Author: FlowKraft
+ * Author URI: https://reportburster.com/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: reportburster-integration
  * Domain Path: languages
  *
  */
+
+// Add to reportburster-integration.php (near the top)
+require_once plugin_dir_path(__FILE__) . 'debug.php';
 
 if (!defined('ABSPATH')) {
   exit();
@@ -33,3 +36,4 @@ if (!defined('ABSPATH')) {
 */
 
 require_once __DIR__ . '/bootstrap/autoload.php';
+
