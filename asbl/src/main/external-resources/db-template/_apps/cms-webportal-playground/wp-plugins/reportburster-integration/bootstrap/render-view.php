e3261f5e88f0fad212b24ef201abcfbeacc5dd22
FlowKraft <flowkraft@reportburster.com>
2025-07-05 20:28:30 +0200

    ----------

--- asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/bootstrap/render-view.php
+++ asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/bootstrap/render-view.php
@@ -1,17 +1,28 @@
 <?php
+<?php
 
 /**
- * This file is loaded by the 'single_template' and 'page_template' filters.
- * Its job is to call the correct Blade view based on the WordPress query.
+ * This file is loaded by template filters to render Blade views.
  */
 
+//blade support for WordPress pages
 if (is_page()) {
     $page_slug = get_post_field('post_name', get_queried_object_id());
     $view_name = "page-{$page_slug}"; // e.g., 'page-portal-dashboard'
     echo view($view_name);
 
-} elseif (is_singular()) {
+} elseif (is_singular()) { //blade support for WordPress CPTs
     $post_type = get_post_type();
     $view_name = "single-{$post_type}"; // e.g., 'single-invoice'
     echo view($view_name);
+    
+} elseif ($pod_name = get_query_var('pods')) { //blade support for WordPress PODs
+    // Handle ACT-Pod templates
+    $slug = get_query_var('slug');
+    $pod = pods($pod_name, $slug);
+    
+    if ($pod) {
+        $view_name = "single-{$pod_name}";
+        echo view($view_name, ['pod' => $pod]);
+    }
 }
