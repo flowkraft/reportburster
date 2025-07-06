7cab7a8c44843a9b85cfa1f68f8bd238f167277d
sourcekraft <virgil.trasca@gmail.com>
2025-07-06 23:28:00 +0200

    -

--- asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/bootstrap/render-view.php
+++ asbl/src/main/external-resources/db-template/_apps/cms-webportal-playground/wp-plugins/reportburster-integration/bootstrap/render-view.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 
 /**
  * This file is loaded by template filters to render Blade views.
@@ -10,10 +9,8 @@ if (is_page()) {
     $page_slug = get_post_field('post_name', get_queried_object_id());
     $view_name = "page-{$page_slug}"; // e.g., 'page-portal-dashboard'
     echo view($view_name);
-
 } elseif (is_singular()) { //blade support for WordPress CPTs
     $post_type = get_post_type();
     $view_name = "single-{$post_type}"; // e.g., 'single-invoice'
     echo view($view_name);
-    
 }
